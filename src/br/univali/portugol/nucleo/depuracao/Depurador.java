d940cfc6d37e86f25174b318ef28f6cda97f13c6
Fillipi Domingos Pelz <fillipi.pelz@gmail.com>
2012-10-30 21:28:19 -0200

    depurador

--- src/br/univali/portugol/nucleo/depuracao/Depurador.java
+++ src/br/univali/portugol/nucleo/depuracao/Depurador.java
@@ -20,6 +20,7 @@ import br.univali.portugol.nucleo.asa.NoDeclaracaoVetor;
 import br.univali.portugol.nucleo.asa.NoDecremento;
 import br.univali.portugol.nucleo.asa.NoEnquanto;
 import br.univali.portugol.nucleo.asa.NoEscolha;
+import br.univali.portugol.nucleo.asa.NoExpressao;
 import br.univali.portugol.nucleo.asa.NoFacaEnquanto;
 import br.univali.portugol.nucleo.asa.NoIncremento;
 import br.univali.portugol.nucleo.asa.NoInteiro;
@@ -32,6 +33,7 @@ import br.univali.portugol.nucleo.asa.NoPara;
 import br.univali.portugol.nucleo.asa.NoPare;
 import br.univali.portugol.nucleo.asa.NoPercorra;
 import br.univali.portugol.nucleo.asa.NoReal;
+import br.univali.portugol.nucleo.asa.NoReferencia;
 import br.univali.portugol.nucleo.asa.NoReferenciaMatriz;
 import br.univali.portugol.nucleo.asa.NoReferenciaVariavel;
 import br.univali.portugol.nucleo.asa.NoReferenciaVetor;
@@ -42,11 +44,15 @@ import br.univali.portugol.nucleo.asa.Operacao;
 import br.univali.portugol.nucleo.asa.Quantificador;
 import br.univali.portugol.nucleo.asa.TipoDado;
 import br.univali.portugol.nucleo.asa.VisitanteASA;
+import br.univali.portugol.nucleo.execucao.erros.ErroIndiceVetorInvalido;
 import br.univali.portugol.nucleo.simbolos.Funcao;
 import br.univali.portugol.nucleo.simbolos.Matriz;
+import br.univali.portugol.nucleo.simbolos.Ponteiro;
+import br.univali.portugol.nucleo.simbolos.Simbolo;
 import br.univali.portugol.nucleo.simbolos.TabelaSimbolos;
 import br.univali.portugol.nucleo.simbolos.Variavel;
 import br.univali.portugol.nucleo.simbolos.Vetor;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -55,6 +61,9 @@ import java.util.List;
  */
 public class Depurador implements VisitanteASA
 {
+    
+    
+    private String ultimaReferenciaAcessada;
 
     private TabelaSimbolos tabelaSimbolosGlobal;
     
@@ -93,14 +102,83 @@ public class Depurador implements VisitanteASA
     @Override
     public Object visitar(NoChamadaFuncao chamadaFuncao) throws ExcecaoVisitaASA
     {
-       throw new UnsupportedOperationException("Not supported yet.");
+        Funcao funcao = null;// chamadaFuncao.getNome();
         
+        TabelaSimbolos tabelaSimbolosFuncao = new TabelaSimbolos();
+
+        List<NoExpressao> listaParametrosPassados = chamadaFuncao.getParametros();
+        List<NoDeclaracaoParametro> listaParametrosEsperados = funcao.getParametros();
+
+        for (int i = 0; i < listaParametrosEsperados.size(); i++)
+        {
+            NoExpressao parametroPassado = listaParametrosPassados.get(i);
+            NoDeclaracaoParametro parametroEsperado = listaParametrosEsperados.get(i);
+
+            switch (parametroEsperado.getModoAcesso())
+            {
+                case POR_VALOR:
+                    passarParametroFuncaoPorValor(parametroPassado, parametroEsperado, tabelaSimbolosFuncao);
+                    break;
+                case POR_REFERENCIA:
+                    passarParametroFuncaoPorReferencia(parametroPassado, parametroEsperado, tabelaSimbolosFuncao);
+                    break;
+            }
+        }
+
+        return interpretarListaBlocos(funcao.getBlocos());
     }
 
+     private void passarParametroFuncaoPorReferencia(NoExpressao parametroPassado, NoDeclaracaoParametro parametroEsperado, TabelaSimbolos tabelaSimbolosFuncao)
+    {
+        NoReferencia referencia = (NoReferencia) parametroPassado;
+        String nome = referencia.getNome();
+        Simbolo simbolo =null;// obterSimbolo(nome, tabelaSimbolos);
+
+        tabelaSimbolosFuncao.adicionar(new Ponteiro(parametroEsperado.getNome(), simbolo));
+    }
+    
+     @SuppressWarnings("unchecked")
+    private void passarParametroFuncaoPorValor(NoExpressao parametroPassado, NoDeclaracaoParametro parametroEsperado, TabelaSimbolos tabelaSimbolosFuncao) throws ExcecaoVisitaASA
+    {
+        String nome = parametroEsperado.getNome();
+
+        if (parametroPassado instanceof NoReferenciaVariavel)
+        {
+            NoReferenciaVariavel referencia = (NoReferenciaVariavel) parametroPassado;
+            Simbolo simbolo = null;//extrairSimbolo(obterSimbolo(referencia.getNome(), tabelaSimbolos));
+            tabelaSimbolosFuncao.adicionar(simbolo.copiar(nome));
+        }
+        else
+        {
+            Quantificador quantificador = parametroEsperado.getQuantificador();
+            TipoDado tipoDado = parametroEsperado.getTipoDado();
+            Object valor = parametroPassado.aceitar(this);
+
+            if (quantificador == Quantificador.VALOR)
+            {
+                tabelaSimbolosFuncao.adicionar(new Variavel(nome, tipoDado, valor));
+            }
+            else
+            {
+                if (quantificador == Quantificador.VETOR)
+                {
+                    tabelaSimbolosFuncao.adicionar(new Vetor(nome, tipoDado, (List<Object>) valor));
+                }
+                else
+                {
+                    if (quantificador == Quantificador.MATRIZ)
+                    {
+                        //tabelaSimbolos.adicionar(new Matriz(nome, tipoDado, (List<List<Object>>) valor));
+  
+                    }
+                }
+            }
+        }
+    }
+    
     @Override
     public Object visitar(NoDeclaracaoFuncao declaracaoFuncao) throws ExcecaoVisitaASA
-    {
-        
+    {        
         String nome = declaracaoFuncao.getNome();
         TipoDado tipoDados = declaracaoFuncao.getTipoDado();
         Quantificador quantificador = declaracaoFuncao.getQuantificador();
@@ -305,7 +383,27 @@ public class Depurador implements VisitanteASA
     @Override
     public Object visitar(NoMatriz noMatriz) throws ExcecaoVisitaASA
     {
-        throw new UnsupportedOperationException("Not supported yet.");
+        
+        List<List<Object>> valores = noMatriz.getValores();
+
+        if (valores != null)
+        {
+            int linhas = valores.size();
+
+            for (int i = 0; i < linhas; i++)
+            {
+                List<Object> vetor = valores.get(i);
+
+                int colunas = (vetor == null) ? 0 : vetor.size();
+
+                for (int j = 0; j < colunas; j++)
+                {
+                    vetor.set(j, ((NoExpressao) vetor.get(j)).aceitar(this));
+                }
+            }
+        }
+
+        return valores;
     }
 
     @Override
@@ -344,13 +442,31 @@ public class Depurador implements VisitanteASA
     @Override
     public Object visitar(NoPara noPara) throws ExcecaoVisitaASA
     {
-        throw new UnsupportedOperationException("Not supported yet.");
+         Object valorRetorno = null;
+         
+        //tabelaSimbolos.empilharEscopo();
+        noPara.getInicializacao().aceitar(this);
+        NoExpressao condicao = noPara.getCondicao();
+
+        while ((condicao != null) ? (Boolean) condicao.aceitar(this) : true)
+        {
+            if ((valorRetorno = interpretarListaBlocos(noPara.getBlocos())) != null)
+            {
+                break;
+            }
+
+            noPara.getIncremento().aceitar(this);
+        }
+
+        //tabelaSimbolos.desempilharEscopo();
+
+        return (valorRetorno instanceof NoPare) ? null : valorRetorno;
     }
 
     @Override
     public Object visitar(NoPare noPare) throws ExcecaoVisitaASA
     {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return noPare;
     }
 
     @Override
@@ -362,43 +478,86 @@ public class Depurador implements VisitanteASA
     @Override
     public Object visitar(NoReal noReal) throws ExcecaoVisitaASA
     {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return noReal.getValor();
     }
 
     @Override
     public Object visitar(NoReferenciaMatriz noReferenciaMatriz) throws ExcecaoVisitaASA
     {
-        throw new UnsupportedOperationException("Not supported yet.");
+        int linha = (Integer) noReferenciaMatriz.getLinha().aceitar(this);
+        int coluna = (Integer) noReferenciaMatriz.getColuna().aceitar(this);
+
+        Matriz matriz = null;// = extrairSimbolo(obterSimbolo(nome, tabelaSimbolos));
+        
+        return matriz.getValor(linha, coluna);
     }
 
     @Override
     public Object visitar(NoReferenciaVariavel noReferenciaVariavel) throws ExcecaoVisitaASA
     {
-        throw new UnsupportedOperationException("Not supported yet.");
+        Variavel variavel = null; //noReferenciaVariavel.getNome();
+        return variavel.getValor();
     }
 
     @Override
     public Object visitar(NoReferenciaVetor noReferenciaVetor) throws ExcecaoVisitaASA
     {
-        throw new UnsupportedOperationException("Not supported yet.");
+        
+        Vetor vetor = null; //noReferenciaVetor.getNome()));
+        int indice = (Integer) noReferenciaVetor.getIndice().aceitar(this);
+
+        try
+        {
+            return vetor.getValor(indice);
+        }
+        catch (ArrayIndexOutOfBoundsException aioobe)
+        {
+            throw new ExcecaoVisitaASA(new ErroIndiceVetorInvalido(vetor.getTamanho(), indice, vetor.getNome()),null,noReferenciaVetor);
+        }
     }
 
     @Override
     public Object visitar(NoRetorne noRetorne) throws ExcecaoVisitaASA
     {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return noRetorne.getExpressao().aceitar(this);
     }
 
     @Override
     public Object visitar(NoSe noSe) throws ExcecaoVisitaASA
     {
-        throw new UnsupportedOperationException("Not supported yet.");
+        boolean condicao = (Boolean) noSe.getCondicao().aceitar(this);
+
+        List<NoBloco> blocos = (condicao) ? noSe.getBlocosVerdadeiros() : noSe.getBlocosFalsos();
+
+        //tabelaSimbolos.empilharEscopo();
+        Object valorRetorno = interpretarListaBlocos(blocos);//, tabelaSimbolos);
+        //tabelaSimbolos.desempilharEscopo();
+
+        return valorRetorno;
     }
 
     @Override
     public Object visitar(NoVetor noVetor) throws ExcecaoVisitaASA
     {
-        throw new UnsupportedOperationException("Not supported yet.");
+        List<Object> valoresVetor = noVetor.getValores();
+        List<Object> valores = new ArrayList<Object>(noVetor.getValores().size());
+
+        if (valores != null)
+        {
+            for (int i = 0; i < valoresVetor.size(); i++)
+            {
+                try
+                {
+                    valores.add(((NoExpressao) valoresVetor.get(i)).aceitar(this));//, tabelaSimbolos));
+                }
+                catch (ArrayIndexOutOfBoundsException aioobe)
+                {
+                    throw new ExcecaoVisitaASA( new ErroIndiceVetorInvalido(valores.size(), i, ultimaReferenciaAcessada), null, noVetor);
+                }
+            }
+        }
+
+        return valores;
     }
 
     @Override
