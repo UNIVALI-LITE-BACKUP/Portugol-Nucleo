35eda329148d57c2a6e59929a874c47bbd7ea17e
Fillipi Domingos Pelz <fillipi.pelz@gmail.com>
2013-03-05 15:06:26 -0300

    evitar NullPointerException.

--- src/br/univali/portugol/nucleo/analise/sintatica/AnalisadorSintatico.java
+++ src/br/univali/portugol/nucleo/analise/sintatica/AnalisadorSintatico.java
@@ -98,47 +98,50 @@ public final class AnalisadorSintatico implements ObservadorParsing
             portugolParser.adicionarObservadorParsing(this);
             ArvoreSintaticaAbstrata asa = portugolParser.parse();
             
-            NoDeclaracaoFuncao aguarde = new NoDeclaracaoFuncao("aguarde", TipoDado.VAZIO, Quantificador.VALOR);
-            NoDeclaracaoParametro intervalo = new NoDeclaracaoParametro("intervalo", TipoDado.INTEIRO, Quantificador.VALOR, ModoAcesso.POR_VALOR);
-            List<NoDeclaracaoParametro> parametrosAguarde =  new ArrayList<NoDeclaracaoParametro>();
-            parametrosAguarde.add(intervalo);
-            aguarde.setParametros(parametrosAguarde);
-            asa.getListaDeclaracoesGlobais().add(aguarde);
+            if (asa != null) {
             
-            NoDeclaracaoFuncao limpa = new NoDeclaracaoFuncao("limpa", TipoDado.VAZIO, Quantificador.VALOR);
-            limpa.setParametros(new ArrayList<NoDeclaracaoParametro>());
-            asa.getListaDeclaracoesGlobais().add(limpa);
-            
-            NoDeclaracaoFuncao sorteia = new NoDeclaracaoFuncao("sorteia", TipoDado.INTEIRO, Quantificador.VALOR);
-            List<NoDeclaracaoParametro> parametrosSorteia = new ArrayList<NoDeclaracaoParametro>();
-            NoDeclaracaoParametro limite = new NoDeclaracaoParametro("limite", TipoDado.INTEIRO, Quantificador.VALOR, ModoAcesso.POR_VALOR);
-            parametrosSorteia.add(limite);
-            sorteia.setParametros(parametrosSorteia);
-            asa.getListaDeclaracoesGlobais().add(sorteia);
-            
-            NoDeclaracaoFuncao potencia = new NoDeclaracaoFuncao("potencia", TipoDado.REAL, Quantificador.VALOR);
-            NoDeclaracaoParametro base = new NoDeclaracaoParametro("base", TipoDado.REAL, Quantificador.VALOR, ModoAcesso.POR_VALOR);
-            NoDeclaracaoParametro expoente = new NoDeclaracaoParametro("expoente", TipoDado.REAL, Quantificador.VALOR, ModoAcesso.POR_VALOR);
-            List<NoDeclaracaoParametro> parametrosPotencia = new ArrayList<NoDeclaracaoParametro>();
-            parametrosPotencia.add(base);
-            parametrosPotencia.add(expoente);
-            potencia.setParametros(parametrosPotencia);
-            asa.getListaDeclaracoesGlobais().add(potencia);
-            
-            NoDeclaracaoFuncao raiz = new NoDeclaracaoFuncao("raiz", TipoDado.REAL, Quantificador.VALOR);
-            List<NoDeclaracaoParametro> parametrosRaiz = new ArrayList<NoDeclaracaoParametro>();
-            NoDeclaracaoParametro valor = new NoDeclaracaoParametro("valor", TipoDado.REAL, Quantificador.VALOR, ModoAcesso.POR_VALOR);
-            parametrosRaiz.add(valor);
-            raiz.setParametros(parametrosRaiz);
-            asa.getListaDeclaracoesGlobais().add(raiz);
-            
-            NoDeclaracaoFuncao tamanho = new NoDeclaracaoFuncao("tamanho", TipoDado.INTEIRO, Quantificador.VALOR);
-            List<NoDeclaracaoParametro> parametrosTamanho = new ArrayList<NoDeclaracaoParametro>();
-            NoDeclaracaoParametro vetor = new NoDeclaracaoParametro("vetor", TipoDado.VAZIO, Quantificador.VETOR, ModoAcesso.POR_REFERENCIA);
-            parametrosTamanho.add(vetor);
-            tamanho.setParametros(parametrosTamanho);
-            asa.getListaDeclaracoesGlobais().add(tamanho);
-            
+                NoDeclaracaoFuncao aguarde = new NoDeclaracaoFuncao("aguarde", TipoDado.VAZIO, Quantificador.VALOR);
+                NoDeclaracaoParametro intervalo = new NoDeclaracaoParametro("intervalo", TipoDado.INTEIRO, Quantificador.VALOR, ModoAcesso.POR_VALOR);
+                List<NoDeclaracaoParametro> parametrosAguarde =  new ArrayList<NoDeclaracaoParametro>();
+                parametrosAguarde.add(intervalo);
+                aguarde.setParametros(parametrosAguarde);
+                asa.getListaDeclaracoesGlobais().add(aguarde);
+
+                NoDeclaracaoFuncao limpa = new NoDeclaracaoFuncao("limpa", TipoDado.VAZIO, Quantificador.VALOR);
+                limpa.setParametros(new ArrayList<NoDeclaracaoParametro>());
+                asa.getListaDeclaracoesGlobais().add(limpa);
+
+                NoDeclaracaoFuncao sorteia = new NoDeclaracaoFuncao("sorteia", TipoDado.INTEIRO, Quantificador.VALOR);
+                List<NoDeclaracaoParametro> parametrosSorteia = new ArrayList<NoDeclaracaoParametro>();
+                NoDeclaracaoParametro limite = new NoDeclaracaoParametro("limite", TipoDado.INTEIRO, Quantificador.VALOR, ModoAcesso.POR_VALOR);
+                parametrosSorteia.add(limite);
+                sorteia.setParametros(parametrosSorteia);
+                asa.getListaDeclaracoesGlobais().add(sorteia);
+
+                NoDeclaracaoFuncao potencia = new NoDeclaracaoFuncao("potencia", TipoDado.REAL, Quantificador.VALOR);
+                NoDeclaracaoParametro base = new NoDeclaracaoParametro("base", TipoDado.REAL, Quantificador.VALOR, ModoAcesso.POR_VALOR);
+                NoDeclaracaoParametro expoente = new NoDeclaracaoParametro("expoente", TipoDado.REAL, Quantificador.VALOR, ModoAcesso.POR_VALOR);
+                List<NoDeclaracaoParametro> parametrosPotencia = new ArrayList<NoDeclaracaoParametro>();
+                parametrosPotencia.add(base);
+                parametrosPotencia.add(expoente);
+                potencia.setParametros(parametrosPotencia);
+                asa.getListaDeclaracoesGlobais().add(potencia);
+
+                NoDeclaracaoFuncao raiz = new NoDeclaracaoFuncao("raiz", TipoDado.REAL, Quantificador.VALOR);
+                List<NoDeclaracaoParametro> parametrosRaiz = new ArrayList<NoDeclaracaoParametro>();
+                NoDeclaracaoParametro valor = new NoDeclaracaoParametro("valor", TipoDado.REAL, Quantificador.VALOR, ModoAcesso.POR_VALOR);
+                parametrosRaiz.add(valor);
+                raiz.setParametros(parametrosRaiz);
+                asa.getListaDeclaracoesGlobais().add(raiz);
+
+                NoDeclaracaoFuncao tamanho = new NoDeclaracaoFuncao("tamanho", TipoDado.INTEIRO, Quantificador.VALOR);
+                List<NoDeclaracaoParametro> parametrosTamanho = new ArrayList<NoDeclaracaoParametro>();
+                NoDeclaracaoParametro vetor = new NoDeclaracaoParametro("vetor", TipoDado.VAZIO, Quantificador.VETOR, ModoAcesso.POR_REFERENCIA);
+                parametrosTamanho.add(vetor);
+                tamanho.setParametros(parametrosTamanho);
+                asa.getListaDeclaracoesGlobais().add(tamanho);
+
+            }
             return asa;
         }
         catch (RecognitionException excecao)
