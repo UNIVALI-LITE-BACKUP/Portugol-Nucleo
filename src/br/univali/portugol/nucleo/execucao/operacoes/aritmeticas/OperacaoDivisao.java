a8cb8f79cbcbffc99d77205a1a09f11e5b0109fc
Luiz Fernando Noschang <noschang@univali.br>
2013-11-20 14:16:22 -0200

    Resolvido problema da divisao por zero. Issue #50

--- src/br/univali/portugol/nucleo/execucao/operacoes/aritmeticas/OperacaoDivisao.java
+++ src/br/univali/portugol/nucleo/execucao/operacoes/aritmeticas/OperacaoDivisao.java
@@ -1,24 +1,41 @@
 package br.univali.portugol.nucleo.execucao.operacoes.aritmeticas;
 
+import br.univali.portugol.nucleo.execucao.erros.ErroDivisaoPorZero;
 import br.univali.portugol.nucleo.execucao.operacoes.Operacao;
 
 public class OperacaoDivisao extends Operacao
 {
-    
-    public Integer executar(Integer operandoEsquerdo, Integer operandoDireito){
+    public Integer executar(Integer operandoEsquerdo, Integer operandoDireito)
+    {
         return operandoEsquerdo / operandoDireito;
     }
     
-    public Double executar(Integer operandoEsquerdo, Double operandoDireito){
+    public Double executar(Integer operandoEsquerdo, Double operandoDireito) throws ErroDivisaoPorZero
+    {
+        verificarDivisaoZero(operandoDireito);
+        
         return operandoEsquerdo / operandoDireito;
     }
     
-    public Double executar(Double operandoEsquerdo, Integer operandoDireito){
+    public Double executar(Double operandoEsquerdo, Integer operandoDireito) throws ErroDivisaoPorZero
+    {
+        verificarDivisaoZero(operandoDireito);
+        
         return operandoEsquerdo / operandoDireito;
     }
     
-    public Double executar(Double operandoEsquerdo, Double operandoDireito){
+    public Double executar(Double operandoEsquerdo, Double operandoDireito) throws ErroDivisaoPorZero
+    {
+        verificarDivisaoZero(operandoDireito);
+        
         return operandoEsquerdo / operandoDireito;
     }
     
+    private void verificarDivisaoZero(Object operandoDireito) throws ErroDivisaoPorZero
+    {
+        if (operandoDireito.equals(0) || operandoDireito.equals(0.0))
+        {
+            throw new ErroDivisaoPorZero();
+        }
+    }    
 }
