906090c93a3e23ef33754879acc4666451a65a17
elieserdejesus <elieserdejesus@gmail.com>
2016-09-20 14:07:44 -0300

    Implementando maior ou igual

--- src/br/univali/portugol/nucleo/asa/NoOperacaoLogicaMaiorIgual.java
+++ src/br/univali/portugol/nucleo/asa/NoOperacaoLogicaMaiorIgual.java
@@ -1,17 +1,19 @@
 package br.univali.portugol.nucleo.asa;
 
-
 /**
- * Esta enumeração representa a operação <code>maior-igual</code> no código fonte.
+ * Esta enumeração representa a operação <code>maior-igual</code> no código
+ * fonte.
  * <p>
- * A operação <code>maior-igual</code> serve para verificar se uma expressão é maior ou igual a outra.
- * No Portugol, a operação <code>maior-igual</code> é representada no código fonte pelo operador "&gt;=".
- * A operação <code>maior-igual</code> é uma operação lógica, e portanto, ao ser avaliada retorna um
- * valor lógico: <code>verdadeiro</code> se a expressão à esquerda do operador for maior ou igual à expressão
- * à direita do operador, caso contrário retorna <code>falso</code>.Pode ser utilizada para realizar o 
- * controle de laços de repetição e desvios condicionais.
+ * A operação <code>maior-igual</code> serve para verificar se uma expressão é
+ * maior ou igual a outra. No Portugol, a operação <code>maior-igual</code> é
+ * representada no código fonte pelo operador "&gt;=". A operação
+ * <code>maior-igual</code> é uma operação lógica, e portanto, ao ser avaliada
+ * retorna um valor lógico: <code>verdadeiro</code> se a expressão à esquerda do
+ * operador for maior ou igual à expressão à direita do operador, caso contrário
+ * retorna <code>falso</code>.Pode ser utilizada para realizar o controle de
+ * laços de repetição e desvios condicionais.
  * </p>
- * 
+ *
  * @author Luiz Fernando Noschang
  * @version 1.13
  */
@@ -21,7 +23,22 @@ public final class NoOperacaoLogicaMaiorIgual extends NoOperacaoLogica
     {
         super(operandoEsquerdo, operandoDireito);
     }
-    
+
+    @Override
+    public Boolean evaluate(VisitanteASA visitor) throws ExcecaoVisitaASA
+    {
+
+        Object opEsq = getOperandoEsquerdo().aceitar(visitor);
+        Object opDir = getOperandoDireito().aceitar(visitor);
+
+        if (opEsq instanceof Number && opDir instanceof Number)
+        {
+            return ((Number) opEsq).doubleValue() >= ((Number) opDir).doubleValue();
+        }
+
+        throw new ExcecaoVisitaASA("Tipo inválido na operação lógica 'maior'", null, this);
+    }
+
     /**
      * {@inheritDoc }
      */
--- src/br/univali/portugol/nucleo/execucao/Interpretador.java
+++ src/br/univali/portugol/nucleo/execucao/Interpretador.java
@@ -27,7 +27,6 @@ public abstract class Interpretador implements VisitanteASA
     protected Map<String, Biblioteca> bibliotecas = new TreeMap<>();
 
     private final OperacaoDivisao operacaoDivisao = new OperacaoDivisao();
-    private final OperacaoLogicaMaiorIgual operacaoLogicaMaiorIgual = new OperacaoLogicaMaiorIgual();
     private final OperacaoLogicaMenor operacaoLogicaMenor = new OperacaoLogicaMenor();
     private final OperacaoLogicaMenorIgual operacaoLogicaMenorIgual = new OperacaoLogicaMenorIgual();
     private final OperacaoModulo operacaoModulo = new OperacaoModulo();
@@ -993,17 +992,7 @@ public abstract class Interpretador implements VisitanteASA
     @Override
     public Object visitar(NoOperacaoLogicaMaiorIgual noOperacao) throws ExcecaoVisitaASA
     {
-        try
-        {
-            Object opEsq = noOperacao.getOperandoEsquerdo().aceitar(this);
-            Object opDir = noOperacao.getOperandoDireito().aceitar(this);
-
-            return operacaoLogicaMaiorIgual.executar(noOperacao, opEsq, opDir);
-        }
-        catch (ErroExecucao ex)
-        {
-            throw new ExcecaoVisitaASA(ex, asa, noOperacao);
-        }
+        return noOperacao.evaluate(this);
     }
 
     @Override
