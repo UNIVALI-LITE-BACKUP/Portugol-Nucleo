903cb6ff2bafdb81e2826453b9fa0b45838b4b77
elieserdejesus <elieserdejesus@gmail.com>
2016-10-18 10:22:20 -0200

    Refatorando - separando código das operações em uma classe helper

--- src/br/univali/portugol/nucleo/execucao/gerador/GeradorCodigoJava.java
+++ src/br/univali/portugol/nucleo/execucao/gerador/GeradorCodigoJava.java
@@ -21,6 +21,7 @@ public class GeradorCodigoJava
     private final GeradorChamadaMetodo geradorChamadaMetodo = new GeradorChamadaMetodo();
     private final GeradorSwitchCase geradorSwitchCase = new GeradorSwitchCase();
     private final GeradorDeclaracaoMetodo geradorDeclaracaoMetodo = new GeradorDeclaracaoMetodo();
+    private final GeradorOperacao geradorOperacao = new GeradorOperacao();
 
     public void gera(ASAPrograma asa, PrintWriter saida, String nomeClasseJava) throws ExcecaoVisitaASA, IOException
     {
@@ -216,187 +217,129 @@ public class GeradorCodigoJava
             return null;
         }
 
-        private boolean operandosSaoStrings(NoExpressao a, NoExpressao b)
-        {
-            return a.getTipoResultante() == TipoDado.CADEIA
-                    && b.getTipoResultante() == TipoDado.CADEIA;
-        }
-
-        private boolean usaOperadorPadrao(NoOperacao no, boolean operandosSaoStrings)
-        {
-            if (no instanceof NoOperacaoLogicaIgualdade || no instanceof NoOperacaoLogicaDiferenca)
-            {
-                return !operandosSaoStrings;
-            }
-
-            return true;
-        }
-
-        private void geraCodigoComParenteses(NoOperacao no) throws ExcecaoVisitaASA
-        {
-            if (no.estaEntreParenteses())
-            {
-                saida.append("(");
-            }
-
-            boolean operandosSaoStrings = operandosSaoStrings(no.getOperandoEsquerdo(), no.getOperandoDireito());
-            boolean usaOperadorPadrao = usaOperadorPadrao(no, operandosSaoStrings);
-
-            boolean precisaDeNegacao = !usaOperadorPadrao && (no instanceof NoOperacaoLogicaDiferenca);
-            if (precisaDeNegacao)
-            {
-                saida.append("!"); // not equals
-            }
-
-            no.getOperandoEsquerdo().aceitar(this);
-
-            if (usaOperadorPadrao)
-            {
-                String operador = OPERADORES.get(no.getClass());
-                assert (operador != null);
-                saida.format(" %s ", operador);
-            }
-            else
-            {
-                saida.append(".equals(");
-            }
-
-            no.getOperandoDireito().aceitar(this);
-
-            if (!usaOperadorPadrao)
-            {
-                saida.append(")"); // fecha o parênteses do .equals()
-            }
-
-            if (no.estaEntreParenteses())
-            {
-                saida.append(")");
-            }
-        }
-
         @Override
         public Void visitar(NoOperacaoBitwiseLeftShift no) throws ExcecaoVisitaASA
         {
-            geraCodigoComParenteses(no);
+            geradorOperacao.gera(no, saida, this, asa);
             return null;
         }
 
         @Override
         public Void visitar(NoOperacaoBitwiseRightShift no) throws ExcecaoVisitaASA
         {
-            geraCodigoComParenteses(no);
+            geradorOperacao.gera(no, saida, this, asa);
             return null;
         }
 
         @Override
         public Void visitar(NoOperacaoBitwiseE no) throws ExcecaoVisitaASA
         {
-            geraCodigoComParenteses(no);
+            geradorOperacao.gera(no, saida, this, asa);
             return null;
         }
 
         @Override
         public Void visitar(NoOperacaoBitwiseXOR no) throws ExcecaoVisitaASA
         {
-            geraCodigoComParenteses(no);
+            geradorOperacao.gera(no, saida, this, asa);
             return null;
         }
 
         @Override
         public Void visitar(NoOperacaoBitwiseOu no) throws ExcecaoVisitaASA
         {
-            geraCodigoComParenteses(no);
+            geradorOperacao.gera(no, saida, this, asa);
             return null;
         }
 
         @Override
         public Void visitar(NoOperacaoSoma no) throws ExcecaoVisitaASA
         {
-            geraCodigoComParenteses(no);
+            geradorOperacao.gera(no, saida, this, asa);
             return null;
         }
 
         @Override
         public Void visitar(NoOperacaoDivisao no) throws ExcecaoVisitaASA
         {
-            geraCodigoComParenteses(no);
+            geradorOperacao.gera(no, saida, this, asa);
             return null;
         }
 
         @Override
         public Void visitar(NoOperacaoModulo no) throws ExcecaoVisitaASA
         {
-            geraCodigoComParenteses(no);
+            geradorOperacao.gera(no, saida, this, asa);
             return null;
         }
 
         @Override
         public Void visitar(NoOperacaoSubtracao no) throws ExcecaoVisitaASA
         {
-            geraCodigoComParenteses(no);
+            geradorOperacao.gera(no, saida, this, asa);
             return null;
         }
 
         @Override
         public Void visitar(NoOperacaoMultiplicacao no) throws ExcecaoVisitaASA
         {
-            geraCodigoComParenteses(no);
+            geradorOperacao.gera(no, saida, this, asa);
             return null;
         }
 
         @Override
         public Void visitar(NoOperacaoLogicaOU no) throws ExcecaoVisitaASA
         {
-            geraCodigoComParenteses(no);
+            geradorOperacao.gera(no, saida, this, asa);
             return null;
         }
 
         @Override
         public Void visitar(NoOperacaoLogicaE no) throws ExcecaoVisitaASA
         {
-            geraCodigoComParenteses(no);
+            geradorOperacao.gera(no, saida, this, asa);
             return null;
         }
 
         @Override
         public Void visitar(NoOperacaoLogicaDiferenca no) throws ExcecaoVisitaASA
         {
-            geraCodigoComParenteses(no);
+            geradorOperacao.gera(no, saida, this, asa);
             return null;
         }
 
         @Override
         public Void visitar(NoOperacaoLogicaIgualdade no) throws ExcecaoVisitaASA
         {
-            geraCodigoComParenteses(no);
+            geradorOperacao.gera(no, saida, this, asa);
             return null;
         }
 
         @Override
         public Void visitar(NoOperacaoLogicaMaior no) throws ExcecaoVisitaASA
         {
-            geraCodigoComParenteses(no);
+            geradorOperacao.gera(no, saida, this, asa);
             return null;
         }
 
         @Override
         public Void visitar(NoOperacaoLogicaMaiorIgual no) throws ExcecaoVisitaASA
         {
-            geraCodigoComParenteses(no);
+            geradorOperacao.gera(no, saida, this, asa);
             return null;
         }
 
         @Override
         public Void visitar(NoOperacaoLogicaMenor no) throws ExcecaoVisitaASA
         {
-            geraCodigoComParenteses(no);
+            geradorOperacao.gera(no, saida, this, asa);
             return null;
         }
 
         @Override
         public Void visitar(NoOperacaoLogicaMenorIgual no) throws ExcecaoVisitaASA
         {
-            geraCodigoComParenteses(no);
+            geradorOperacao.gera(no, saida, this, asa);
             return null;
         }
 
@@ -944,35 +887,6 @@ public class GeradorCodigoJava
          * ou função
          * @return Um nome que não conflite com as palavras reservadas do java
          */
-    }
-
-    private static final Map<Class, String> OPERADORES = new HashMap<>();
-
-    static
-    {
-        OPERADORES.put(NoOperacaoAtribuicao.class, "=");
-        OPERADORES.put(NoOperacaoDivisao.class, "/");
-        OPERADORES.put(NoOperacaoModulo.class, "%");
-        OPERADORES.put(NoOperacaoMultiplicacao.class, "*");
-        OPERADORES.put(NoOperacaoSoma.class, "+");
-        OPERADORES.put(NoOperacaoSubtracao.class, "-");
-
-        OPERADORES.put(NoMenosUnario.class, "-");
-
-        OPERADORES.put(NoOperacaoLogicaDiferenca.class, "!=");
-        OPERADORES.put(NoOperacaoLogicaIgualdade.class, "==");
-        OPERADORES.put(NoOperacaoLogicaMaior.class, ">");
-        OPERADORES.put(NoOperacaoLogicaMaiorIgual.class, ">=");
-        OPERADORES.put(NoOperacaoLogicaMenor.class, "<");
-        OPERADORES.put(NoOperacaoLogicaMenorIgual.class, "<=");
-        OPERADORES.put(NoOperacaoLogicaOU.class, "||");
-        OPERADORES.put(NoOperacaoLogicaE.class, "&&");
-
-        OPERADORES.put(NoOperacaoBitwiseE.class, "&");
-        OPERADORES.put(NoOperacaoBitwiseOu.class, "|");
-        OPERADORES.put(NoOperacaoBitwiseXOR.class, "^");
-        OPERADORES.put(NoOperacaoBitwiseLeftShift.class, "<<");
-        OPERADORES.put(NoOperacaoBitwiseRightShift.class, ">>");
     }
 
 }
---
+++ src/br/univali/portugol/nucleo/execucao/gerador/helpers/GeradorOperacao.java
@@ -0,0 +1,101 @@
+package br.univali.portugol.nucleo.execucao.gerador.helpers;
+
+import br.univali.portugol.nucleo.asa.*;
+import br.univali.portugol.nucleo.asa.VisitanteASA;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Elieser
+ */
+public class GeradorOperacao
+{
+    private static final Map<Class, String> OPERADORES = new HashMap<>();
+
+    public void gera(NoOperacao no, PrintWriter saida, VisitanteASA visitor, ASAPrograma asa) throws ExcecaoVisitaASA
+    {
+        if (no.estaEntreParenteses())
+        {
+            saida.append("(");
+        }
+
+        boolean operandosSaoStrings = operandosSaoStrings(no.getOperandoEsquerdo(), no.getOperandoDireito());
+        boolean usaOperadorPadrao = usaOperadorPadrao(no, operandosSaoStrings);
+
+        boolean precisaDeNegacao = !usaOperadorPadrao && (no instanceof NoOperacaoLogicaDiferenca);
+        if (precisaDeNegacao)
+        {
+            saida.append("!"); // not equals
+        }
+
+        no.getOperandoEsquerdo().aceitar(visitor);
+
+        if (usaOperadorPadrao)
+        {
+            String operador = OPERADORES.get(no.getClass());
+            assert (operador != null);
+            saida.format(" %s ", operador);
+        }
+        else
+        {
+            saida.append(".equals(");
+        }
+
+        no.getOperandoDireito().aceitar(visitor);
+
+        if (!usaOperadorPadrao)
+        {
+            saida.append(")"); // fecha o parênteses do .equals()
+        }
+
+        if (no.estaEntreParenteses())
+        {
+            saida.append(")");
+        }
+    }
+
+    private static boolean operandosSaoStrings(NoExpressao a, NoExpressao b)
+    {
+        return a.getTipoResultante() == TipoDado.CADEIA
+                && b.getTipoResultante() == TipoDado.CADEIA;
+    }
+
+    private static boolean usaOperadorPadrao(NoOperacao no, boolean operandosSaoStrings)
+    {
+        if (no instanceof NoOperacaoLogicaIgualdade || no instanceof NoOperacaoLogicaDiferenca)
+        {
+            return !operandosSaoStrings;
+        }
+
+        return true;
+    }
+
+    static
+    {
+        OPERADORES.put(NoOperacaoAtribuicao.class, "=");
+        OPERADORES.put(NoOperacaoDivisao.class, "/");
+        OPERADORES.put(NoOperacaoModulo.class, "%");
+        OPERADORES.put(NoOperacaoMultiplicacao.class, "*");
+        OPERADORES.put(NoOperacaoSoma.class, "+");
+        OPERADORES.put(NoOperacaoSubtracao.class, "-");
+
+        OPERADORES.put(NoMenosUnario.class, "-");
+
+        OPERADORES.put(NoOperacaoLogicaDiferenca.class, "!=");
+        OPERADORES.put(NoOperacaoLogicaIgualdade.class, "==");
+        OPERADORES.put(NoOperacaoLogicaMaior.class, ">");
+        OPERADORES.put(NoOperacaoLogicaMaiorIgual.class, ">=");
+        OPERADORES.put(NoOperacaoLogicaMenor.class, "<");
+        OPERADORES.put(NoOperacaoLogicaMenorIgual.class, "<=");
+        OPERADORES.put(NoOperacaoLogicaOU.class, "||");
+        OPERADORES.put(NoOperacaoLogicaE.class, "&&");
+
+        OPERADORES.put(NoOperacaoBitwiseE.class, "&");
+        OPERADORES.put(NoOperacaoBitwiseOu.class, "|");
+        OPERADORES.put(NoOperacaoBitwiseXOR.class, "^");
+        OPERADORES.put(NoOperacaoBitwiseLeftShift.class, "<<");
+        OPERADORES.put(NoOperacaoBitwiseRightShift.class, ">>");
+    }
+
+}
